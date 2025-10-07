bfd094cbd6bfd18d73b38f0c8ff7aa3d1d94239d
Jakub F. Bortlík <jakub.bortlik@proton.me>
2025-10-07 19:28:55 +0200

    feat: add --verbose command-line argument

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 
-def main(prompt):
+def main(prompt, verbose):
     client = genai.Client(api_key=api_key)
+
+    messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=prompt)])]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Error: You must provide a prompt.")
         sys.exit(1)
-    prompt = " ".join(sys.argv[1:])
-    main(prompt)
+    prompt = sys.argv[1]
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose = True
+    else:
+        verbose = False
+    main(prompt, verbose)
